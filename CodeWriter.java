a88f2e9502ef61d37a5b3c4b250cdaa4d420addf
Vipin Kumar <vipin.ind.kumar@gmail.com>
2017-08-16 12:49:40 +0000

    Implement setFileName() with some changes in constructor

--- CodeWriter.java
+++ CodeWriter.java
@@ -1,18 +1,23 @@
 public class CodeWriter {
     
     public BufferedWriter out;
+    public String file;
     
     /** Open the output file, and 
      * get ready to write
      **/
     public CodeWriter(outFile) {
         out = new BufferedWriter(new FileWriter(new File(outFile)));
+        if (outFile.contains(".vm") {
+            outFile = outFile.split(".vm")[0];
+        }
+        setFileName(outFile);
     }
     
     // Informs the codeWriter that
     // the translation of a new file has started
     public setFileName(fileName) {
-        
+        file = fileName;
     }
     
     // write the assembly code for given 
